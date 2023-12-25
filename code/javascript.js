363df71dcbe268736292f5fffccfbea167952a68
GabrielDevJourney <gabspereira178@gmail.com>
2023-12-25 11:33:17 +0000

    add variables to script to get values from html

--- code/javascript.js
+++ code/javascript.js
@@ -1,57 +1,61 @@
-    let optionsArray = ["rock", "paper", "scissors"];
     let playerScore = 0;
     let computerScore = 0;
+    let roundWinner = '';
 
-    function getComputerChoice() {
-    return optionsArray[Math.floor(Math.random() * optionsArray.length)];
-    }
 
     function playRound(playerSelection, computerSelection) {
-    //HERE WE JUST LOG THE CHOICES
-    console.log(`Player chose: ${playerSelection}`);
-    console.log(`Computer chose: ${computerSelection}`);
-
     if (playerSelection === computerSelection) {
-        console.log("It's a tie! Let's play again");
-        return true;
-    } else if (
+
+        roundWinner = 'tie';
+    }
+    if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        playerScore += 1;
-        return false; //this is used so the loop for the playagain doenst happen
-    } else {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        computerScore += 1;
-        return false; //this is used so the loop for the playagain doenst happen
+
+        playerScore++
+        roundWinner = 'player'
     }
+    if (
+        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+    ){
+
+        computerScore++;
+        roundWinner = "computer";
+    }
+    updateScoreMessage(roundWinner,playerSelection,computerSelection)
     }
 
-    let isTie = true;
-
-    while (isTie) {
-    const playerSelection = prompt(
-        "Enter your choice: Rock, Paper, Scissors."
-    ).toLowerCase();
-    const computerSelection = getComputerChoice();
-    isTie = playRound(playerSelection, computerSelection);
+    function getRandomChoice() {
+      let randomNumber = Math.floor(Math.random() * 3);
+        switch (randomNumber) {
+            case 0:
+            return "rock";
+            case 1:
+            return "paper";
+            case 2:
+            return "scissors";
+        }
     }
 
-    function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt(
-        "Enter your choice: Rock, Paper, Scissors."
-        ).toLowerCase();
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
+    function gameOver() {
+        return playerScore ===3 || computerScore === 3
     }
 
-    if (playerScore >= 3) {
-        console.log(`You won with ${playerScore} - ${computerScore}`);
-    } else {
-        console.log(`You lost ${playerScore} - ${computerScore} `);
-    }
-    }
-    game();
+    //ui get variable from html to js script
+    const scoreInfo = document.getElementById("scoreInfo");
+    const scoreMessage = document.getElementById("scoreMessage");
+    const playerScorePara = document.getElementById("playerScore");
+    const computerScorePara = document.getElementById("computerScore");
+    const playerSign = document.getElementById("playerWeapon");
+    const computerSign = document.getElementById("computerWeapon");
+    const rockBtn = document.getElementById("rockBtn");
+    const paperBtn = document.getElementById("paperBtn");
+    const scissorsBtn = document.getElementById("scissorsBtn");
+    const endgameModal = document.getElementById("endgameModal");
+    const endgameMsg = document.getElementById("endgameMsg");
+    const overlay = document.getElementById("overlay");
+    const restartBtn = document.getElementById("restartBtn");
